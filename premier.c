789b2b5026c457f9e158a34fef2b2a10dc83a70e
theophane16 <theophane.paradis007@gmail.com>
2023-08-12 02:24:37 +0200

    jusqu'aux nombres aléatoires

--- premier.c
+++ premier.c
@@ -3,6 +3,8 @@
 #include <string.h>     // si on veut utiliser des strings
 #include <math.h>      // pour utiliser des fonctions mathematiques
 #include <ctype.h>
+#include <stdlib.h>     // 1er pour nb aléatoires
+#include <time.h>       // 2eme pour nb aléatoires
 
 
 //////////////// functions + arguments + return statement //////////////////////
@@ -20,10 +22,64 @@ int findMax(int x, int y){
     return (x > y) ? x : y;             // Donc : si x est supérieur à y --> on retourne x, sinon on retourne y
 }
 
+void sort(int array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[i])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d",array[i]);
+    }
+    
+}
+
+
+
 //////////////// function prototype //////////////////////
 void hello(char[],int);                     // --> ici on previent le programme que l'on va définir cette méthode (après le main), si l'on veut l'appeler on peut mais il faudra respecter le type des arguments
                                             // POINT IMPORTANT : Many compilers do not check the parameter matching // Missing argument will result in unexpected behavior // A function prototype causes the compiler to flag if arguments are missing
                                             // ADVANTAGES --> Easier to  navigate a program // Helps with debugging // Commonly used in header files
+
+
+//////////////// structs //////////////////////
+    struct Player
+    {
+        char name[12];
+        int score;
+    };
+    
+//////////////// typedef //////////////////////
+    typedef struct 
+    {
+        char name[25];
+        char password[12];
+        int id;
+    } User;
+    
+
+//////////////// enums //////////////////////
+    enum Day{Sun = 1, Mon = 2, Tue = 3, Wed = 4, Thu = 5, Fri = 6, Sat = 7};
+
+
+
+
+
+
+
 
 
 ////////////////////////////////////////////
@@ -366,9 +422,178 @@ int main(){
 
 //////////////// nested loops //////////////////////
     // a loop inside another loop
-    // Cours 30 : 2:01:35
+    int rows;
+    int colums;
+    char symbol;
+
+    for(int i = 1; i <= rows; i++){
+        for (int j = 1; i <= colums; i++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+//////////////// break and continue //////////////////////
+    // continue = skips rest of code & forces the next iteration of the loop
+    // break = exits a loop
+    for (int i = 0; i <= 20; i++)
+    {
+        if (i == 23)
+        {
+            continue;
+            // ou break;        --> sors de la boucle
+        }
+
+    }
+
+//////////////// arrays //////////////////////
+    // array = a data structure that can store many values of the same data type
+
+    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
+    printf("%.2lf", prices[2]);
+
+    // Pour l'afficher
+    for (int i = 0; i < 5; i++)
+    {
+       printf("%.2lf\n", prices[i]);
+    }
+
+    printf("%d bytes\n", sizeoff(prices));      // --> donne 48 bits ici
+
+    for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++)              // ici on aurait pu prendre la taille d'un autre élément
+    {
+        printf("&%.2lf\n", prices[i]);
+    }
+
+//////////////// 2D Arrays //////////////////////
+    // 2D array = an array where each elements is an entire array --> useful if you need a matrix, grid, or table of data
+    int numbers[2][3] = {
+        {1, 2, 3},
+        {1, 2, 3}
+        };
+    
+    // ou
+
+    int numbers[2][3];
+
+    int rows = sizeof(numbers) / sizeof(numbers[0]);
+    int columns = sizeof(numbers[0]) / sizeof(numbers[0][0]);
+
+
+    int numbers[0][0] = 1;
+    int numbers[0][1] = 1;
+    int numbers[0][2] = 1;
+    int numbers[1][0] = 1;
+    int numbers[1][1] = 1;
+    int numbers[1][2] = 1;
+
+    // ou
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d", numbers[i][j]);
+        }
+        printf("\n");
+    }
+    
+
+//////////////// array of strings //////////////////////
+    char cars[][10] = {"Mustang","Corvette","Camaro"};                  // on peut voir ça comme un tableau de tableau de caractères
+    strcpy(cars[0], "Tesla");       // --> avoir le #include <string.h>
+
+    for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++)
+    {
+        printf("%s\n", cars[i]);
+    }
+
+//////////////// swap values of two variables //////////////////////
+    char x[15] = 'X';
+    char y[15] = 'Y';
+    char tempo[15];
+
+    strcpy(tempo, x);
+    strcpy(x, y);
+    strcpy(y, tempo);
+
+//////////////// sort an array //////////////////////
+    int array[] = {9, 2, 9, 8, 5, 6, 4};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array, size);                      // --> marche avec les cacractères aussi
+    printArray(array, size);
+
+
+//////////////// structs //////////////////////
+    // struct = collection of related members ("variables"), they can be of different data types listed under one name in a block of memory --> VERY SIMILAR to classes in other languages (but NO METHODS)
+    // Voir avant le main()
+    struct Player player1;
+    struct Player player2;
+
+    strcpy(player1.name, "Bro");
+    player1.score = 4;
+
+    strcpy(player2.name, "Bru");
+    player2.score = 5;
+
+    printf("%s",player1.name);
+    printf("%s",player1.score);
+
+//////////////// typedef //////////////////////
+    // typedef = reserved keyword that gives an existing datatype a " nickname"
+    // voir avant le main
+    User user1 = {"Bro", "password123", 1216548};
+    User user2 = {"Bra", "password183", 1254545};
+
+
+//////////////// Array of structs //////////////////////
+    // on réutilise Player (dans "structs")
+    struct Player player01 = { "Super", 5.0};
+    struct Player player02 = { "Franky", 4.0};
+    struct Player player03 = { "Sunny", 8.0};
+    struct Player player04 = { "Brook", 7.0};
+
+    struct Player players[] = {player01, player02, player03, player04};
+
+    for (int i = 0; i < sizeof(players)/sozeof(players[0]); i++)
+    {
+        printf("%-12s\t", players[i].name);
+        printf("%.2f\n", players[i].gpa);
+    }
+    
+//////////////// enums //////////////////////
+    // enums = a user defined type af named integer identifiers // help to make a program more readable
+    // voir avant le main()
+    enum Day today = Sat;
+
+    if(today == Sun || today == Sat)                        // sert à renommer (ici au lieu d'avoir 1 et 7 on a Sun et Sat)
+    {
+        printf("\nIt's the weekend ! Party time");          // enums are not Strings but they can treated as int
+    }
+    else 
+    {
+        printf("I have to work today:(");
+    }
+    
+//////////////// random numbers //////////////////////
+    srand(time(0));
+
+    int number001 = (rand() % 20) +1;           // on peut changer le nombre après le %
+
+    printf("%d\n", number001);
+
+//////////////// number guessing game (à faire) //////////////////////
+
+
 
     
+
+
+
+
+
 
 
 
